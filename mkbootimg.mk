f4a9e873952e710885e42ced490b65df811e1893
Varun Chitre <varun.chitre15@gmail.com>
2015-04-14 06:27:55 -0400

    m9-common: cleanup the mkbootimg mess
    *. Don't create the ancient age dtb file list, dtbTool does that on it's own
    *. Force v2 dtb type
    
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,38 +1,11 @@
-LOCAL_PATH := $(call my-dir)
-PERL = perl
-
-## Don't change anything under here. The variables are named M9_whatever
-## on purpose, to avoid conflicts with similarly named variables at other
-## parts of the build environment
-
-## Imported from the original makefile...
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-
-M9_DTS_NAMES ?= $(shell $(PERL) -e 'while (<>) {$$a = $$1 if /CONFIG_ARCH_((?:MSM|QSD|MPQ)[a-zA-Z0-9]+)=y/; $$r = $$1 if /CONFIG_MSM_SOC_REV_(?!NONE)(\w+)=y/; $$arch = $$arch.lc("$$a$$r ") if /CONFIG_ARCH_((?:MSM|QSD|MPQ)[a-zA-Z0-9]+)=y/} print $$arch;' $(KERNEL_CONFIG))
-M9_DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/qcom/$(M9_DTS_NAME)*.dts)
-M9_DTS_FILE = $(lastword $(subst /, ,$(1)))
-DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm64/boot/dts/,$(patsubst %.dts,%.dtb,$(call M9_DTS_FILE,$(1))))
-ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm64/boot/,$(patsubst %.dts,%-Image,$(call M9_DTS_FILE,$(1))))
-KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm64/boot/Image
-DTC = $(KERNEL_OUT)/scripts/dtc/dtc
-
-define append-m9-dtb
-$(foreach M9_DTS_NAME, $(M9_DTS_NAMES), \
-   $(foreach d, $(M9_DTS_FILES), \
-      $(DTC) -p 1024 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
-      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
-endef
-
-
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-	$(call append-m9-dtb)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm64/boot/dts/
+	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm64/boot/dts/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
 
