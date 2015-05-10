c5a205c6b6c7871dc5b6b170810b3efd89ea3265
Rashed Abdel-Tawab <rashed@linux.com>
2015-05-10 15:17:08 -0400

    [TEMP] hima: Use prebuilt libril.so

--- system_prop.mk
+++ system_prop.mk
@@ -7,9 +7,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
     persist.sys.isUsbOtgEnabled=true
 
-# QC RIL
+# HTC RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+	rild.libpath=/system/lib64/libhtc_rilhook.so
+
+# QC RIL (disabled for now, using prebuilt libril)
+#PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+#	rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
