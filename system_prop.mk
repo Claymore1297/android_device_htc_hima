2fd2979d2e13d3cc4e650efc6467fa8e0153ca22
Rashed Abdel-Tawab <rashed@linux.com>
2015-05-10 19:36:44 -0400

    hima: system_prop: Fix properties for RIL

--- system_prop.mk
+++ system_prop.mk
@@ -83,8 +83,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.apm_sim_not_pwdn=1
+#    persist.radio.add_power_save=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=false \
