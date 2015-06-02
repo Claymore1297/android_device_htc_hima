ae5f7d606b648b306194e324472abdd565e45125
Varun Chitre <varun.chitre15@gmail.com>
2015-06-02 15:11:37 -0400

    hima : disable nfc hal compilation for now

--- hima.mk
+++ hima.mk
@@ -172,7 +172,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
-    nfc_nci.pn54x.default \
+#    nfc_nci.pn54x.default \
     Tag
 
 PRODUCT_COPY_FILES += \
