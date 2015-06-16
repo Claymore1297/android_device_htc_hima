7db18ea3bb894112b65c2d1dbe9ad171a788468f
Varun Chitre <varun.chitre15@gmail.com>
2015-06-16 10:56:58 -0400

    hima : fix bootdevice symlink issue
    We need to explicitly supply the base platform device path
    
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,6 +149,9 @@ TARGET_POWERHAL_VARIANT := qcom
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
 
+# Init
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/htc/hima-common/sepolicy
