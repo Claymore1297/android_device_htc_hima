8e9a48d160427b1bdeca269a03cb919638f88399
Rashed Abdel-Tawab <rashed@linux.com>
2015-07-13 07:19:50 -0400

    hima: Support checking if the keymaster app is loaded
    Change-Id: I3ce68df2cdf098f9d81f2889cd47b48fce454ba4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,6 +128,9 @@ EXTENDED_FONT_FOOTPRINT := true
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
+# Keymaster app checking
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
