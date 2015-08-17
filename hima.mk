ce4e4f8a084282d0e410fdda6485686caf58ca86
rc420head <dennislebhardt@yahoo.com>
2015-08-16 19:36:43 -0500

    touchscreen: add support for max1187x
    some m9's shipped with a max1187x touchscreen
    users with max1187x end up with a cursor and touch screen acts as a mouse
    this brings back normal touchscreen operation
    
    to see what screen you have run
    
    cat /sys/class/input/input*/name
    
    in a terminal emulator or adb shell
    
    this fix comes from xda member wickedmx
    
    ps2:fix typo
    
    ps3:clean up whitespace & update commit message
    
    ps4:WHITESPACE!!!!
    Change-Id: I363f88c585ec9b6d76e6924b390a999a396f10c3

--- hima.mk
+++ hima.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
     memtrack.msm8994 \
     liboverlay
 
+# max1187x touchscreen support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/max1187x_touchscreen_0.idc:system/usr/idc/max1187x_touchscreen_0.idc
+
 # Keymaster
 PRODUCT_PACKAGES += \
     keystore.msm8994
