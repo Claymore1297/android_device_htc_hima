0e588847365dd8f460aa867ec3d1da857a373d25
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-10-25 12:01:19 +1030

    hima: minor cosmetic camerawrapper change
    Probably the least useful change I'll ever contribute, may aswell do it now.
    
    Change-Id: I92b3be09c0994a5911a5096fe4dad4d90dc38363

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -337,7 +337,7 @@ static char *camera_get_parameters(struct camera_device *device)
     if (!device)
         return NULL;
 
-    char *parameters = VENDOR_CALL(device, get_parameters); 
+    char *parameters = VENDOR_CALL(device, get_parameters);
     wrapper_camera_device_t *wrapper = (wrapper_camera_device_t *)device;
 
     CameraParameters params;
