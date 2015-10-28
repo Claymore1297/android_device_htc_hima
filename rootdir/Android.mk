e8d506041508673a75dd4caf2a1a9a63d747da2b
Ethan Chen <intervigil@gmail.com>
2015-10-27 22:23:41 -0700

    hima: Remove unused coex script
    * Broadcom WLAN/BT doesn't use this
    
    Change-Id: I589d17934ba9ecca34cef68ab27d4510b3a48aa6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -46,13 +46,6 @@ include $(BUILD_PREBUILT)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.coex.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.coex.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.fm.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
