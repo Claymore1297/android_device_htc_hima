664ab90358152438c91f1d76d24822511089db48
Ethan Chen <intervigil@gmail.com>
2015-11-27 22:34:46 -0800

    hima: Build stlport
    * Required for L libmmcamera_hdr_gb_lib.so
    
    Change-Id: Ia03c2845862d18e2baa56b306746c24d0219f883

--- hima.mk
+++ hima.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Compat symbols
+PRODUCT_PACKAGES += \
+    libstlport
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8994 \
