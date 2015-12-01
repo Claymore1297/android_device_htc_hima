fb1388e310856859ae33d3af4c399e0b8b5fc09f
Ethan Chen <intervigil@gmail.com>
2015-11-30 21:23:54 -0800

    hima: Add camera shim library
    Change-Id: Ia0321e686405e9a00754d3260a555b86ec1d82f9

--- hima.mk
+++ hima.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libcamera_shim
+
 # Compat symbols
 PRODUCT_PACKAGES += \
     libstlport
