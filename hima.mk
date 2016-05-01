aaa8926758efbf76968cb4b47749d1202a19c287
Rashed Abdel-Tawab <rashed@linux.com>
2016-05-01 01:05:38 -0400

    hima: Gello time
    Change-Id: I1b9c9143e1f1f350e8dfa491b37d3b97a60bc798

--- hima.mk
+++ hima.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Dotcase
 
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
