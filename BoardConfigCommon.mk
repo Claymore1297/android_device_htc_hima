e84d0b17355c10f0591822670e5b5442404e9efc
Christopher N. Hesse <raymanfx@gmail.com>
2016-09-17 19:15:09 +0200

    hima: Use sensor manager API compatibility hack
    Change-Id: I33a2bd84d721fb98111811aa3b2a69b4ec7cd534

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 
+# Sensors
+BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
