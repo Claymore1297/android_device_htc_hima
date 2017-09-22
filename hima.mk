688bbda890b2b2b87832d244693c172e2d9093c4
Ethan Chen <intervigil@gmail.com>
2017-09-22 00:48:41 -0700

    hima: Support DRM properly
    * Switch to hwbinder DRM service
    * Declare entries in manifest
    * Switch to bullhead Widevine library list
    
    Change-Id: Iec7bb7e141924219530875df3c0c727c7f3b75a5

--- hima.mk
+++ hima.mk
@@ -127,6 +127,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Dotcase
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Gesture Handler
 PRODUCT_PACKAGES += \
     GestureHandler
