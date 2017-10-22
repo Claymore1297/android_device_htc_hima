bc9ed274924877fb9a064a3bf46102f250e6bd45
Christopher N. Hesse <raymanfx@gmail.com>
2017-10-22 17:55:00 +0200

    hima: s/cm/lineage/
    Change-Id: Ia02701924aa09c40f9440e1d7573e40e5320e342

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,9 +24,9 @@ export INITIAL_COPYRIGHT_YEAR=2015
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -33,7 +34,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" "true"
 
 # Copyright headers and guards
 write_headers "himaul himawl himawhl"
@@ -46,7 +47,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
     # Copyright headers and guards
     write_headers
