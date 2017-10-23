d0bd373fb9843691c8867a08d93bc469e2a2cc63
Christopher N. Hesse <raymanfx@gmail.com>
2017-10-23 19:05:00 +0200

    hima: Use Lineage legacy camera provider
    Change-Id: I02665bb99213f4182e74926ff9865cbe1e9fbd93

--- hima.mk
+++ hima.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
     libshim_camera \
     Snap
 
