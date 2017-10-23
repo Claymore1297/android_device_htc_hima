833ee0198bc793c33a3930bf138b0eb6d3e60a64
Christopher N. Hesse <raymanfx@gmail.com>
2017-10-23 18:33:45 +0200

    hima: Disable sdcardfs until the kernel impl is done
    Change-Id: I3d1af64601444a099d5315358e1ba8dcc5f2efa6

--- system_prop.mk
+++ system_prop.mk
@@ -98,6 +98,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=false
 
+# SDCard FS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=false
+
 # Storage
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
