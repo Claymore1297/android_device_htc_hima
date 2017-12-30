11119811b945d4abf8fbb789597105316cb330e3
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    hima-common: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c

--- hima.mk
+++ hima.mk
@@ -248,9 +248,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8994
+    android.hardware.power@1.0-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
