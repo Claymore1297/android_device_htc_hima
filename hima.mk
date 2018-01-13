df618b95cf4514912dfd012e127708c56388d1f0
Julian Veit <claymore1298@gmail.com>
2018-01-13 10:05:34 +0100

    hima: Remove audiod
    this replaces https://review.lineageos.org/#/c/201302 due a merge
    conflict

--- hima.mk
+++ hima.mk
@@ -76,7 +76,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8994 \
     audio.r_submix.default \
