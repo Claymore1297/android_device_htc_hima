36fff2f39a3980971728d275ec0901bfc5e8d97f
Paul Keith <javelinanddart@gmail.com>
2018-02-19 18:21:29 +0100

    hima-common: Use sdFAT for exFAT
    Change-Id: I660c5b3008636a651a2812fe1739ff8a2ea845cb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,7 +98,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Extended filesystem support
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
