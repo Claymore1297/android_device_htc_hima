b99520f87fc0044ca80289583aa4bcd86cf92993
Paul Keith <javelinanddart@gmail.com>
2018-02-20 14:48:59 +0100

    hima-common: Update kernel exfat flag
    Change-Id: Ic182471b28ec91340b13cdc6cad64a64772f4641

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Extended filesystem support
-TARGET_KERNEL_HAVE_EXFAT := true
+TARGET_EXFAT_DRIVER := exfat
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
