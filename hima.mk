40724b5eb3a2e811240262a73d9400e5070ce5ed
Julian Veit <claymore1298@gmail.com>
2018-02-24 18:30:01 +0100

    Revert "hima: Binderize audio"
    This reverts commit cfe051326a9c4e9774d7ff3ff2ffb7b091cf9fe0.
    
    Change-Id: I81545c0126e8152fe497059291f02e10135529c1

--- hima.mk
+++ hima.mk
@@ -74,9 +74,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8994 \
     audio.r_submix.default \
