2b53d6e330e3a6962420d971b5fb8752118cab82
Julian Veit <claymore1298@gmail.com>
2018-03-20 23:47:07 +0100

    hima-common: overlay-lineage: Apply LineageOS rebrand
    Change-Id: I5677dd4d3c4265cffe939a2e226c2e9d14528170

--- hima.mk
+++ hima.mk
@@ -18,8 +18,9 @@
 $(call inherit-product-if-exists, vendor/htc/hima-common/hima-common-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
