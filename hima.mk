b0b477c0460890b533c72bbe0624ec470ad1fa38
Julian Veit <claymore1298@gmail.com>
2018-05-07 21:20:02 +0200

    hima-common: Build LegacyMM livedisplay HAL

--- hima.mk
+++ hima.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.hima
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-legacymm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/vendor/etc/media_codecs.xml \
