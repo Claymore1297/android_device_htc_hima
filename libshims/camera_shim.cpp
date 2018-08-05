85fa5bbff114435cab1be4a5aab4e28cdada79eb
Ethan Chen <intervigil@gmail.com>
2018-08-05 01:23:39 -0700

    hima-common: Update camera shim
    Change-Id: I16b6c0f45854f6233e986b6d940b18dc5bde5b2a

--- libshims/camera_shim.cpp
+++ libshims/camera_shim.cpp
@@ -14,57 +14,90 @@
  * limitations under the License.
  */
 
+#include <gui/BufferQueue.h>
+#include <gui/SurfaceComposerClient.h>
+#include <utils/Errors.h>
+#include <utils/String8.h>
+#include <utils/StrongPointer.h>
+
 #include <string>
 
-//using namespace android;
+using android::sp;
+using android::status_t;
+using android::IBinder;
+using android::IGraphicBufferConsumer;
+using android::IGraphicBufferProducer;
+using android::PixelFormat;
+using android::SurfaceControl;
+
+// BufferItemConsumer(const sp<IGraphicBufferConsumer& consumer,
+//                    uint64_t consumerUsage,
+//                    int bufferCount = DEFAULT_MAX_BUFFERS,
+//                    bool controlledByApp = false)
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(
+    const sp<IGraphicBufferConsumer>& consumer, uint64_t consumerUsage,
+    int bufferCount, bool controlledByApp);
+
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
+    const sp<IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+    int bufferCount, bool controlledByApp) {
+  _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(
+      consumer, consumerUsage, bufferCount, controlledByApp);
+}
+
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
+    sp<IGraphicBufferProducer>* outProducer,
+    sp<IGraphicBufferConsumer>* outConsumer,
+    void*) {
+  // createBufferQueue is a static method, call it directly
+  android::BufferQueue::createBufferQueue(outProducer, outConsumer);
+}
 
 // GraphicBuffer(uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
 //               uint32_t inUsage, std::string requestorName = "<Unknown>");
 extern "C" void _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
-    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage,
+    uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat, uint32_t inUsage,
     std::string requestorName);
 
 extern "C" void _ZN7android13GraphicBufferC1Ejjij(
-    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage) {
+    uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
+    uint32_t inUsage) {
   std::string requestorName = "<Unknown>";
   _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
       inWidth, inHeight, inFormat, inUsage, requestorName);
 }
 
-extern "C" void _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEENS1_INS_22IGraphicBufferProducerEEE(
-    uint32_t token, uint32_t bufferProducer);
-//    const sp<IBinder>& token, sp<IGraphicBufferProducer> bufferProducer);
-
-extern "C" void _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEERKNS1_INS_22IGraphicBufferProducerEEE(
-    uint32_t token, uint32_t bufferProducer) {
-//    const sp<IBinder>& token, const sp<IGraphicBufferProducer>& bufferProducer) {
-  _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEENS1_INS_22IGraphicBufferProducerEEE(
-      token, bufferProducer);
+extern "C" status_t _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEERKNS1_INS_22IGraphicBufferProducerEEE(
+    const sp<IBinder>& token, const sp<IGraphicBufferProducer>& bufferProducer) {
+  // setDisplaySurface is a static method, call it directly
+  return android::SurfaceComposerClient::setDisplaySurface(token, bufferProducer);
 }
 
-extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
-    void *outProducer, void *outConsumer, bool consumerIsSurfaceFlinger);
+// sp<SurfaceControl> createSurface(const String8& name, uint32_t w, uint32_t h,
+//                                  PixelFormat format, uint32_t flags = 0,
+//                                  SurfaceControl* parent = nullptr,
+//                                  uint32_t windowType = 0,
+//                                  uint32_t ownerUid = 0);
+extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
+    const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
+    uint32_t flags, SurfaceControl* parent, uint32_t windowType,
+    uint32_t ownerUid);
 
-extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
-    void *outProducer, void *outConsumer, void *allocator __unused) {
-  _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
-      outProducer, outConsumer, false);
+extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(
+    const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
+    uint32_t flags) {
+  return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
+      name, w, h, format, flags, nullptr, 0, 0);
 }
 
-extern "C" void _ZN7android18BufferItemConsumerC1ENS_2spINS_22IGraphicBufferConsumerEEEjib(
-    uint32_t consumer, uint32_t consumerUsage, int bufferCount, bool controlledByApp) {
-  _ZN7android18BufferItemConsumerC1ENS_2spINS_22IGraphicBufferConsumerEEEjib(
-      consumer, consumerUsage, bufferCount, false);
-}
+// status_t setLayer(int32_t layer);
+/* needed for N-libs
+extern "C" status_t _ZN7android14SurfaceControl8setLayerEi(int32_t layer);
 
-extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib
-(
-     //   const sp<IGraphicBufferConsumer>&consumer, unsigned int, int, bool)
-     uint64_t consumer, uint64_t consumerUsage, int bufferCount, bool controlledByApp){
-     _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
-     consumer, consumerUsage, bufferCount, false);
+extern "C" status_t _ZN7android14SurfaceControl8setLayerEj(uint32_t layer) {
+  return _ZN7android14SurfaceControl8setLayerEi(layer);
 }
-
+*/
 // android::Fence::~Fence()
 extern "C" void _ZN7android5FenceD1Ev() {
   // no-op, the explicit destructor was replaced with = default;
