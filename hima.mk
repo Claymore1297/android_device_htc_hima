466244394b34df9f771d7cc4b9382756e61a29d7
Ethan Chen <intervigil@gmail.com>
2018-08-05 21:37:53 -0700

    hima-common: Convert thermal HAL for hima
    Change-Id: I66a1a331f64ed49c1937e9f64c73cc96c1d1130a

--- hima.mk
+++ hima.mk
@@ -287,7 +287,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
