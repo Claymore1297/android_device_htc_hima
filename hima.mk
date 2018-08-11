1d14f6bd3a27af9b75b57bfe21057ca30570d022
Alberto97 <albertop2197@gmail.com>
2018-08-11 18:22:12 +0200

    hima-common: Move to NFC 1.1 Implementation
    * Introduced in AOSP 9.0. Will require configuration changes.
    
    Change-Id: I2dac6284ac5f330eae23a0d5d16626041886c841
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- hima.mk
+++ hima.mk
@@ -226,11 +226,9 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
+    android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
     NfcNci \
-    nfc_nci.msm8994 \
     Tag
 
 PRODUCT_COPY_FILES += \
