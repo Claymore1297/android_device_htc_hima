555bf3f776d5cbd0ae1d22325fb0720500f24bdb
Bruno Martins <bgcngm@gmail.com>
2018-09-05 23:01:52 +0100

    hima-common: Only include legacy QC sepolicy
     * This has now turned into a separate repository (maintained only
       for legacy devices, those that never got official Orea updates).
    
    Change-Id: I981b452b697bc3610d7aa97b74ed182c6b70ca30

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,9 +169,8 @@ TARGET_RIL_VARIANT := caf
 BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+include device/qcom/sepolicy-legacy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
