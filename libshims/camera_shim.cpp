6de94bbf50ffe38aff5d85467e96d0a3506f7df7
Ethan Chen <intervigil@gmail.com>
2018-09-06 02:00:51 -0700

    hima-common: Add camera shim for Fence destructor
    * This was replaced with a default destructor which has no visible
      linkage. No-op the destructor call and hope the actual destructor is
      called from underneath.
    
    Change-Id: Id039e916c24959e9f60391bc10886df878f4d265

--- libshims/camera_shim.cpp
+++ libshims/camera_shim.cpp
@@ -65,3 +65,8 @@ extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferCo
      consumer, consumerUsage, bufferCount, false);
 }
 
+// android::Fence::~Fence()
+extern "C" void _ZN7android5FenceD1Ev() {
+  // no-op, the explicit destructor was replaced with = default;
+}
+
