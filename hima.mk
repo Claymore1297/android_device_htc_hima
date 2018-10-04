5c214bee2a60318458561853246bb1e571b9fbc9
Joel Stein <js@funkturm.net>
2018-10-04 02:02:04 +0200

    hima-common: nfc: use 1.0 service, prebuilt impl
    Use LOS15.1 prebuilt impl
    Current implementation doesn't support pn547
    Check https://review.lineageos.org/#/c/LineageOS/android_hardware_nxp_nfc/+/223194/
    for efforts reimplementing support.
    
    Change-Id: I642fd13d235c5295c35222aa14b4afd4dd9dfcdc

--- hima.mk
+++ hima.mk
@@ -230,17 +230,21 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
     android.hardware.nfc@1.0-service \
     com.android.nfc_extras \
     NfcNci \
-    nfc_nci.msm8994 \
+    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+    $(LOCAL_PATH)/prebuilts/nfc/lib/nfc_nci.msm8994.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/nfc_nci.msm8994.so \
+    $(LOCAL_PATH)/prebuilts/nfc/lib64/nfc_nci.msm8994.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/nfc_nci.msm8994.so \
+    $(LOCAL_PATH)/prebuilts/nfc/lib/android.hardware.nfc@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.nfc@1.0-impl.so \
+    $(LOCAL_PATH)/prebuilts/nfc/lib64/android.hardware.nfc@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.nfc@1.0-impl.so
+
 
 # OMX
 PRODUCT_PACKAGES += \
