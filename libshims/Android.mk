6102ac046aba49fcb4a756e30dfafc67cedb7e12
Joel Stein <js@funkturm.net>
2018-10-07 23:26:21 +0200

    hima-common: remove PermissionController-hack from camera-shim (not needed for n-libs)
    Change-Id: I841613bd770568cf2228c2852941f5ec575db23b

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -17,8 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    camera_shim.cpp \
-    IPermissionController.cpp
+    camera_shim.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbinder \
