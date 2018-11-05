08ee45507c58456bad6185c0e3359802b5b94a36
Julian Veit <claymore1298@gmail.com>
2018-11-05 22:02:08 +0100

    hima-common: amplifier: Use generated kernel headers
    Change-Id: I29871de87426a306072ae9567eacb435a670499a

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -13,8 +13,7 @@ LOCAL_C_INCLUDES := \
 	hardware/libhardware/include \
 	$(call project-path-for,qcom-audio)/hal
 
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_SRC_FILES := \
 	rt5501.c \
