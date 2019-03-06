609bba9288450d39da1a85c4efe7a4d9dc4e421b
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    hima-common: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,6 +196,7 @@ BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcm4356_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcm4356.bin"
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # inherit from the proprietary version
 -include vendor/htc/hima-common/BoardConfigVendor.mk
