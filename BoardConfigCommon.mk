ce4732f0bd29bd34c2b420aa3a78edac7dfc7a6c
fagyi <fagyipda@gmail.com>
2019-06-09 13:55:58 +0200

    hima-common: enable cpuidle during boot
    This is only used to keep cpuidle disabled during boot, which is
    unnecessary and doesn't really improve boot times (but does incur a high
    power cost).
    
    Change-Id: Ie4641664722449a29d709867fd0cd203661fe186

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x00078000
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 boot_cpus=0-3 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
