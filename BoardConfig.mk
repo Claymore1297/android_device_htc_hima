095c45a396d4c19bfb965895b954ed23448d2cfe
Joel Stein <js@funkturm.net>
2019-10-03 02:25:07 +0200

    hima: clean up OTA_ASSERTS
    We support htc_himauhl, htc_himaulatt, htc_himawhl and htc_himawl
    in unified ROM.
    
    TWRP actually only knows htc_himaul, htc_himawhl and htc_himawl
    Everything that isn't htc_himawhl or htc_himawl is classified as
    htc_himaul in TWRP, so we add it to ASSERTS as well.
    
    Change-Id: Ibd34e7a9adf4f2a3dc88098684f2b68a6afae59d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ TARGET_KERNEL_SOURCE := kernel/htc/msm8994
 TARGET_KERNEL_CONFIG := lineageos_hima_defconfig
 
 # Device Asserts
-TARGET_OTA_ASSERT_DEVICE := htc_himaul,htc_himauhl,htc_himaulatt,himaul,himauhl,himaulatt,htc_himawl,himawl,htc_himawhl,himawhl
+TARGET_OTA_ASSERT_DEVICE := htc_himaul,htc_himauhl,htc_himaulatt,htc_himawhl,htc_himawl
 
 # Audio
 AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
