8ba83df40c4cd0a473a5b9dd61b7a4f499854e8f
Joel Stein <js@funkturm.net>
2019-10-04 00:28:32 +0200

    hima: Add libhidl shim (needed for widevine-1.0)
    Change-Id: I004f072ca12d50137eb86df746cd9cfb203ce818

--- device.mk
+++ device.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey \
+    android.hidl.base@1.0
 
 # Gesture Handler
 PRODUCT_PACKAGES += \
