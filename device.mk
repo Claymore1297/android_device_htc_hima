8908a5475a8ba390918b16ade044fe5318f8683b
Julian Veit <claymore1298@gmail.com>
2021-12-26 11:47:08 +0100

    hima: wipe out telephony-ext
    Change-Id: I06ca4e55644ab8d790325f2dcb363080f89b02a3

--- device.mk
+++ device.mk
@@ -330,12 +330,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libshim_ril
 
-# codeaurora telephony-ext
-PRODUCT_PACKAGES += \
-    telephony-ext
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
